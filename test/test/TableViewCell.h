7e90153a4953aa37e3c1e0a6bd8ad1d7f2bd09a6
cearsjignesh <cears.jignesh@gmail.com>
2017-07-29 11:20:29 +0530

    Add files via upload

---
+++ test/test/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  test
+//
+//  Created by Admin on 29/07/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ test/test/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  test
+//
+//  Created by Admin on 29/07/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
